4aa31f710300ff0870d2995a65f69523a5f39a93
SrMouraSilva <mateus.moura@hotmail.com>
2017-03-26 19:19:10 +0000

    Change threading -> GPIOThread

--- component/sevensegments/multiplex_thread.py
+++ component/sevensegments/multiplex_thread.py
@@ -1,19 +1,21 @@
 from time import sleep
-import threading
+from gpiozero.threads import GPIOThread
 
-
-class MultiplexThread(threading.Thread):
+class MultiplexThread(object):
     stopped = None
 
     def __init__(self, displays_manager):
-        threading.Thread.__init__(self)
+        self._display_thread = GPIOThread(
+            target=self._start,
+            args=()
+        )
 
         self.stopped = False
         self.manager = displays_manager
         self.sleep = 0.005
 
-    def run(self):
-        self._start()
+    def start(self):
+        self._display_thread.start()
 
     def _start(self):
         while not self.stopped:
